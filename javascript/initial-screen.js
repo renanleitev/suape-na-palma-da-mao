6148dbaad2459f1b0b385969691f36c0d0d5c352
renanleitev <renanleitev@gmail.com>
2023-09-30 21:11:29 -0300

    Add: Suporte a itinerario pelo mapa

--- javascript/initial-screen.js
+++ javascript/initial-screen.js
@@ -1,12 +1,16 @@
 // definindo as coordenadas iniciais
 let latitude = -8.394097983524112;
 let longitude = -34.97408204488957;
-// obtendo os elementos html pelo id
+// obtendo os containers de mensagem
 const iframe = document.getElementById("google-maps");
 const loading = document.getElementById("loading");
 const error = document.getElementById("error");
 const warning = document.getElementById("warning");
 const offline = document.getElementById("offline");
+const errorItinerario = document.getElementById("error-itinerario");
+const errorNaoItinerario = document.getElementById("error-nao-itinerario");
+// mensagem padrão do itinerario
+const defaultItineario = document.getElementById("default-itinerario");
 // botão de mostrar informações da empresa
 const btnEmpresa = document.getElementById("btn-empresa");
 // obtendo as informações das empresas
@@ -14,10 +18,14 @@ const empresaNome = document.getElementById('empresa-nome');
 const empresaAtividade = document.getElementById('empresa-atividade');
 const empresaEndereco = document.getElementById('empresa-endereco');
 const empresaContato = document.getElementById('empresa-contato');
-// armazenando as empresas obtidas com a api
-const listaEmpresas = [];
 // delay para exibir os avisos na tela
 const delayTime = 2000;
+// armazenando as empresas obtidas com a api
+let listaEmpresas = [];
+// armazenando as empresas adicionadas ao itinerário
+let listaEmpresasItinerario = [];
+// armazenando as coordenadas das empresas
+let listaEmpresasCoordenadas = [];
 // definindo a url base
 // se estiver usando localmente (comentar/descomentar a linha abaixo)
 // const baseUrl = "http://localhost:3000";
@@ -31,6 +39,7 @@ function getPositionSuccess(position) {
     longitude = position.coords.longitude;
     iframe.src = `https://maps.google.com/maps?q=${latitude},${longitude}&z=15&output=embed`;
 }
+// se não for possível obter a localização
 function getPositionError() {
     warning.showModal();
     setTimeout(() => warning.close(), delayTime);
@@ -39,6 +48,7 @@ function getPositionError() {
 navigator.geolocation.getCurrentPosition(getPositionSuccess, getPositionError);
 // obtendo os dados das empresas
 const initialUrl = `${baseUrl}/empresa/`;
+// parametros para a requisição GET
 const options =
 {
     headers: {
@@ -51,28 +61,176 @@ const options =
 fetch(initialUrl, options)
     .then((resposta) => resposta.json())
     .then(data => {
-        // adicionando nas opções
-        data.forEach(empresa => {
-            listaEmpresas.push(empresa);
+        // checando se há ou não empresas no itinerario
+        checkItinerario();
+        // ordenando os dados em ordem alfabetica e salvando em um array
+        // https://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
+        listaEmpresas = data.sort((a, b) => a.Nome.localeCompare(b.Nome))
+        // adicionando as empresas na lista de opções
+        listaEmpresas.forEach(empresa => {
+            // criando um elemento do tipo option
             const option = document.createElement("option");
+            // o texto de cada option será o nome da empresa
             option.text = empresa.Nome;
+            // o valor de cada option será a latitude e a longitude
             option.value = `${empresa.Latitude},${empresa.Longitude}`;
+            // selecionado o elemento select
             const select = document.getElementById("empresas");
+            // adicionando cada option no select
             select.appendChild(option);
         });
     });
-// Obtendo a url para o iframe
+// checando se há empresas no itinerario
+function checkItinerario() {
+    // selecionando o botão de pesquisar
+    const pesquisarButton = document.getElementById("pesquisar-button");
+    // se não houver nenhuma empresa no itinerario
+    if (listaEmpresasItinerario.length === 0) {
+        // desabilitar o botão de pesquisa
+        pesquisarButton.disabled = true;
+        // a cor de fundo do botão fica cinza
+        pesquisarButton.style.backgroundColor = "gray";
+    } else if (listaEmpresasItinerario.length > 0) {
+        // se houver empresa no itinerario, a pesquisa fica habilitada
+        pesquisarButton.disabled = false;
+        // a cor de fundo do botão fica branca
+        pesquisarButton.style.backgroundColor = "white";
+    }
+    // selecionando o contador de empresas no itinerario
+    const itinerarioCount = document.getElementById("itinerario-count");
+    // o contador será a quantidade de empresas no itinerario
+    itinerarioCount.innerHTML = listaEmpresasItinerario.length;
+}
+// adicionando empresa ao itinerario
+function addToListJourney(empresa) {
+    // removendo a mensagem de nenhum itinerario
+    defaultItineario.innerHTML = "";
+    // adicionando a empresa na lista de itinerario
+    const itinerarioLista = document.getElementById("itinerario-lista");
+    // criando um elemento li (item) para adicionar a lista de itinerario
+    const itinerarioItem = document.createElement("li");
+    // o texto de cada item da lista será o nome da empresa
+    itinerarioItem.innerHTML = empresa.Nome;
+    // o id de cada item da lista será o nome da empresa
+    itinerarioItem.id = empresa.Nome;
+    // adicionando a empresa a lista de itinerario
+    itinerarioLista.appendChild(itinerarioItem);
+}
+// removendo empresa do itinerario
+function removeCompanyFromJourney(empresa) {
+    try {
+        // se o itinerario estiver vazio, exibir mensagem
+        if (listaEmpresasItinerario.length === 0) {
+            defaultItineario.innerHTML = "Nenhum itinerário disponível.";
+        }
+        // removendo a empresa da lista de itinerario
+        document.getElementById(empresa.Nome).remove();
+        // removendo a empresa da lista de coordenadas
+        listaEmpresasCoordenadas = listaEmpresasCoordenadas.filter(coordenadas => coordenadas.lat !== empresa.Latitude);
+    } catch(e) {
+        // se a empresa não estiver no itinerario, exibir mensagem de erro
+        errorNaoItinerario.showModal();
+        setTimeout(() => errorNaoItinerario.close(), delayTime);
+    }
+}
+// salvando os dados das empresas em um array
+function addCompanyToJourney(empresa) {
+    listaEmpresasItinerario.push(empresa);
+    addToListJourney(empresa);
+    const coordenadas = {
+        lat: empresa.Latitude,
+        lon: empresa.Longitude,
+    };
+    listaEmpresasCoordenadas.push(coordenadas);
+}
+// adicionando empresas ao itinerário
+function addCompany() {
+    // obtendo os valores do input de pesquisar
+    const pesquisar = document.getElementById('pesquisar');
+    const consulta = pesquisar.value;
+    // se o input não estiver vazio
+    if (consulta !== ''){
+        // procurando a empresa
+        const regex = new RegExp(consulta, 'gi');
+        const empresaEstaItinerario = listaEmpresasItinerario.find(empresa => empresa.Nome.match(regex));
+        if (empresaEstaItinerario) {
+            // se a empresa já estiver no itinerario, não pode ser adicionada
+            errorItinerario.showModal();
+        } else {
+            const empresaEncontrada = listaEmpresas.find(empresa => empresa.Nome.match(regex));
+            // se a empresa for encontrada
+            if (empresaEncontrada) {
+                addCompanyToJourney(empresaEncontrada);
+                checkItinerario();
+            } else {
+                // se a empresa não for encontrada
+                error.showModal();
+            }
+        }
+    } else {
+        // se o input estiver vazio, obter os valores pelo select
+        const coordenadasEmpresa = document.getElementById("empresas").value;
+        const [empresaLatitude, empresaLongitude] = coordenadasEmpresa.split(',');
+        const empresaEncontrada = listaEmpresas.find((empresa) => empresa.Latitude === empresaLatitude);
+        const regex = new RegExp(empresaEncontrada.Nome, 'gi');
+        const empresaEstaItinerario = listaEmpresasItinerario.find(empresa => empresa.Nome.match(regex));
+        if (empresaEstaItinerario) {
+            // se a empresa já estiver no itinerario, não pode ser adicionada
+            errorItinerario.showModal();
+        } else {
+            addCompanyToJourney(empresaEncontrada);
+            checkItinerario();
+        }
+    }
+}
+// removendo empresas do itinerario
+function removeCompany() {
+    // obtendo os valores do input de pesquisar
+    const pesquisar = document.getElementById('pesquisar');
+    const consulta = pesquisar.value;
+    // se o input não estiver vazio
+    if (consulta !== ''){
+        // procurando a empresa
+        const regex = new RegExp(consulta, 'gi');
+        const empresa = listaEmpresasItinerario.find(empresa => empresa.Nome.match(regex));
+        // se a empresa for encontrada, remover ela
+        if (empresa) {
+            // removendo a empresa do itinerario
+            listaEmpresasItinerario = listaEmpresasItinerario.filter(empresa => !empresa.Nome.match(regex));
+            removeCompanyFromJourney(empresa);
+            checkItinerario();
+        } else {
+            // se a empresa não for encontrada
+            error.showModal();
+        }
+    } else {
+        // se o input estiver vazio, obter os valores pelo select
+        const coordenadasEmpresa = document.getElementById("empresas").value;
+        // separando os valores obtidos pelo valor do select
+        const [empresaLatitude, empresaLongitude] = coordenadasEmpresa.split(',');
+        // encontrando a empresa, pela latitude
+        const empresaEncontrada = listaEmpresasItinerario.find((empresa) => empresa.Latitude === empresaLatitude);
+        // se a empresa for encontrada, remover ela
+        if (empresaEncontrada) {
+            // removendo a empresa do itinerario
+            const regex = new RegExp(empresaEncontrada.Nome, 'gi');
+            listaEmpresasItinerario = listaEmpresasItinerario.filter(empresa => !empresa.Nome.match(regex));
+            removeCompanyFromJourney(empresaEncontrada);
+            checkItinerario();
+        } else {
+            // se a empresa não estiver no itinerario
+            errorNaoItinerario.showModal();
+        }
+    }
+}
+// obtendo a url para o iframe
 // https://medium.com/@supun1001/how-to-generate-google-embed-links-programmatically-for-iframes-for-routes-only-d6dc225e59e8
-function createUrlMap(destinationLatitude, destinationLongitude) {
+function createUrlMap(coordenadas) {
     const origem = {
         lat: latitude,
         lon: longitude
     };
-    const destino = {
-        lat: destinationLatitude,
-        lon: destinationLongitude
-    };
-    const coords = [origem, destino];
+    const coords = [origem, ...coordenadas];
     let coordinateString = '';
     // fazendo o cálculo da rota
     for (const item of coords) {
@@ -83,21 +241,28 @@ function createUrlMap(destinationLatitude, destinationLongitude) {
     urlConstruct += `!1m12!1m3!1d1.0!2d${coords[0].lon}!3d${coords[0].lon}`;
     urlConstruct += `!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1`;
     urlConstruct += `!4m${coords.length * 4 + 1}!3e0${coordinateString}`;
-    urlConstruct += `!5e0!3m2!1sen!2sau!4v${epochNow}000!5m2!1sen!2sau`;
+    urlConstruct += `!5e0!3m2!1spt-BR!2sbr!4v${epochNow}000!5m2!1spt-BR!2sbr`;
     iframe.src = urlConstruct;
 }
-// Exibindo informações da empresa selecionada
+// exibindo informações da empresa selecionada
 function splitPhoneNumber(contato){
+    // separando os números de telefone, se houver mais de um
     const listaContato = contato.split(',');
+    // para cada número de telefone, criar um link para ligar
     listaContato.forEach((telefone) => {
+        // criando um elemento br, para quebrar linha entre os números
         const quebraLinha = document.createElement("br");
+        // criando um elemento a, para criar um link para ligar
         const telefoneNumero = document.createElement("a");
+        // o texto do link será o número de telefone
         telefoneNumero.innerHTML = telefone;
+        // o link será o número de telefone
         telefoneNumero.href = `tel:${telefone}`;
+        // adicionando o número de telefone ao contato da empresa
         empresaContato.appendChild(telefoneNumero);
+        // adicionando a quebra de linha, para separar os números
         empresaContato.appendChild(quebraLinha);
     })
-
 }
 function showCompanyInfo(empresa) {
     empresaNome.innerHTML += empresa.Nome;
@@ -111,54 +276,50 @@ function resetCompanyInfo(){
     empresaEndereco.innerHTML = "Endereço: ";
     empresaContato.innerHTML = "Contato: ";
 }
-// obtendo os valores pelo input
+// realizando a pesquisa
 function searchOption() {
     // mostrando informações da empresa
     btnEmpresa.classList.remove("d-none");
     // resetando as informações da empresa
     resetCompanyInfo();
-    // obtendo os valores do input de pesquisar
-    const pesquisar = document.getElementById('pesquisar');
-    const consulta = pesquisar.value;
-    // definindo a url de consulta
-    const searchUrl = `${baseUrl}/empresa/query/${consulta}`;
-    // se o input não for vazio, pesquisar o texto que o usuário digitou
-    if (consulta !== '') {
-        loading.showModal();
-        fetch(searchUrl, options)
-            .then((resposta) => {
-                if (resposta.status !== 200) {
-                    loading.close();
-                    error.showModal();
-                    setTimeout(() => error.close(), delayTime);
-                    pesquisar.value = '';
-                    return;
-                }
-                return resposta.json();
-            })
-            .then(empresa => {
-                createUrlMap(empresa.Latitude, empresa.Longitude);
-                showCompanyInfo(empresa);
-                pesquisar.value = '';
-                loading.close();
-            })
-            .catch(() => {
-                loading.close();
-                error.showModal();
-                setTimeout(() => error.close(), delayTime);
-            });
+    const select = document.getElementById("empresas");
+    // se não houver nenhuma empresa no select === api offline
+    if (select.options.length === 0) {
+        offline.showModal();
     } else {
-        // se o input estiver vazio, obter os valores pelo select
-        const coordenadasEmpresa = document.getElementById("empresas").value;
-        if (coordenadasEmpresa) {
-            const [empresaLatitude, empresaLongitude] = coordenadasEmpresa.split(',');
-            const empresa = listaEmpresas.find((empresa) => empresa.Latitude === empresaLatitude);
-            showCompanyInfo(empresa);
-            createUrlMap(empresaLatitude, empresaLongitude);
-        } else {
-            error.showModal();
-            setTimeout(() => error.close(), delayTime);
-        }
+        createUrlMap(listaEmpresasCoordenadas);
     }
-
+}
+// fechando os dialogs
+// https://stackoverflow.com/questions/50037663/how-to-close-a-native-html-dialog-when-clicking-outside-with-javascript
+function dialogClickHandler(e) {
+    if (e.target.tagName !== 'DIALOG') {
+        //This prevents issues with forms
+        return;
+    } 
+    const rect = e.target.getBoundingClientRect();
+    const clickedInDialog = (
+        rect.top <= e.clientY &&
+        e.clientY <= rect.top + rect.height &&
+        rect.left <= e.clientX &&
+        e.clientX <= rect.left + rect.width
+    );
+    if (clickedInDialog === false) {
+        e.target.close();
+    }
+}
+function closeWarning(){
+    warning.close();
+}
+function closeError(){
+    error.close();
+}
+function closeErrorItinerario(){
+    errorItinerario.close();
+}
+function closeErrorNaoItinerario(){
+    errorNaoItinerario.close();
+}
+function closeOffline(){
+    offline.close();
 }
